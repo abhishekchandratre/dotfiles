c7671e0aafc3aadfcd954aa3c7292b4e7166cdd4
Abhishek Chandratre <abhishek.chandratre@gmail.com>
2018-01-01 04:32:54 -0500

    Added tmux configuration

--- install.sh
+++ install.sh
@@ -1,9 +1,13 @@
 #!/bin/sh
 
-echo "Updating vimrc"
 if [ ! -d "$HOME/.vim" ]; then
-	echo "Inside"
+	echo "Updating vimrc"
 	cp vimrc ~/.vimrc
 	mkdir -p ~/.vim/autoload
 	cp vim/autoload/plug.vim ~/.vim/autoload
+fi
+
+if [ ! -f "$HOME/.tmux.conf" ]; then
+	echo "Updating tmux.conf"
+	cp tmux.conf ~/.tmux.conf
 fi
